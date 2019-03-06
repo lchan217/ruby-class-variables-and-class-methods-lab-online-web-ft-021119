7c19e0163600c3462f7daeafb955239a310c1aff
Lizzie Chan <lchan217@gmail.com>
2019-03-06 15:14:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,9 @@
 require "pry"
-class Song 
- 
+class Song
+  attr_accessor :name, :artist, :genre 
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+  end 
 end 
