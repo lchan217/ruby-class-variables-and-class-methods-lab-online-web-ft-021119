238b2feb0910bb4037d5c3f7cd29680c32407733
Lizzie Chan <lchan217@gmail.com>
2019-03-06 15:28:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,38 +1,54 @@
 require "pry"
-class Song
+class Song 
   attr_accessor :name, :artist, :genre 
-  @@count = 0
+  @@count = 0 
+  @@genres = [] 
   @@artists = [] 
-  @@genres = []
+  
   def initialize(name, artist, genre)
     @name = name 
-    @artist = artist 
-    @genre = genre 
+    @artist = artist
+    @genre = genre
     @@count += 1 
     @@artists << artist 
     @@genres << genre 
-  end 
+  end
+  
   def self.count 
-    @@count
+    @@count 
   end 
+  
   def self.artists 
     @@artists.uniq
   end 
   def self.genres 
     @@genres.uniq 
   end 
-  def self.genre_count 
-    hash = {}
+
+  def self.genre_count
     counter = 0 
-    @@genres.reverse.each do |element|
-      if hash.include?(element)
+    new_hash = {} 
+    @@genres.reverse.each do |genre|
+      if !new_hash[genre] 
         counter += 1 
+        new_hash[genre] = counter 
       else 
         counter += 1 
-        hash[element] = counter 
+      end
+    end
+    new_hash
+  end 
+  def self.artist_count
+    counter = 0 
+    hash = {} 
+    @@artists.reverse.each do |artist|
+      if !hash[artist]
+        counter += 1 
+        hash[artist] = counter 
+      else 
+        counter += 1 
       end 
     end 
     hash
   end 
-
 end 
